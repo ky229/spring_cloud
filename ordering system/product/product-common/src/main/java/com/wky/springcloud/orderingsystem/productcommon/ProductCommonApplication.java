216b41c2389196a6824317c6840892047fcb0743
kaiyun.wu <kaiyun.wu.z@gmail.com>
2019-07-31 22:29:32 +0800

    搭建product服务分层骨架

---
+++ ordering system/product/product-common/src/main/java/com/wky/springcloud/orderingsystem/productcommon/ProductCommonApplication.java
@@ -0,0 +1,13 @@
+package com.wky.springcloud.orderingsystem.productcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCommonApplication.class, args);
+    }
+
+}
