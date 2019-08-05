5e0117e8bf28e68ee29573e0ade9856baabd4588
wky <kaiyun.wu@qq.com>
2019-08-05 09:19:22 +0800

    【add】编写配置文件和福物价局通信的controller

---
+++ ordering system/order/order-server/src/main/java/com/examplecom/wky/springcloud/orderingsystem/orderserver/contorller/MessageTestController.java
@@ -0,0 +1,12 @@
+package com.examplecom.wky.springcloud.orderingsystem.orderserver.contorller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wky(kaiyun.wu @ qq.com)
+ * @version v1.0
+ * @data 2019/8/4 0004 18:00
+ */
+@RestController
+public class MessageTestController {
+}
